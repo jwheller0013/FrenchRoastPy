b06f7e9fd985188f5fb892838946a3503835f6ca
jwheller0013 <jwheller0013@yahoo.com>
2025-04-02 16:29:28 -0400

    Attempt two seems to work better

---
+++ FrenchToast_2.py
@@ -0,0 +1,43 @@
+import csv
+
+transaction_template = {
+    "account_number": 0,
+    "balance": 0,
+}
+
+customer = {
+}
+
+with open('resources/transactions.csv', 'r', newline='') as file:
+    csv_reader = csv.reader(file)
+    next(csv_reader)
+
+    lines  = 0
+    for line in csv_reader:
+
+        cus = line[1]
+        cusID = int(line[2])
+        transType = line[3].strip().lower()
+        amount = float(line[4])
+        if customer.get(cus) is None:
+            customer[cus] = []
+
+        account_exists = False
+        for account_info in customer[cus]:
+            if account_info["account_number"] == cusID:
+                account_exists = True
+                if transType == 'withdrawal':
+                    account_info["balance"] -= amount
+                elif transType == 'deposit':
+                    account_info["balance"] += amount
+
+        if not account_exists:
+            new_account = transaction_template.copy()
+            new_account["account_number"] = cusID
+            if transType == 'withdrawal':
+                new_account["balance"] -= amount
+            elif transType == 'deposit':
+                new_account["balance"] += amount
+            customer[cus].append(new_account)
+
+    print(customer)
